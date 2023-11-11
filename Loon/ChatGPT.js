19713cd7db49e88c1f304abffebb938d09d4cd85
Asimpleuncle <70625820+Asimpleuncle@users.noreply.github.com>
2023-11-11 16:41:59 +0800

    Update ChatGPT.js

--- Loon/ChatGPT.js
+++ Loon/ChatGPT.js
@@ -1,7 +1,6 @@
 #!name=ChatGPTPlusCrack
 #!desc=ChatGPTPlusCrack
-#!icon=https://github.com/Toperlock/Quantumult/raw/main/icon/Doraemon/Doraemon-1044.png
-
+#!icon=https://raw.githubusercontent.com/mgxray/Quantumult_x_II/main/country/chatgpt.png
 [Script]
 
 http-response ^https?:\/\/ios\.chat\.openai\.com\/backend-api\/(accounts\/check\/|models) script-path=https://raw.githubusercontent.com/yqc007/QuantumultX/master/ChatGPTPlusCrack.js, requires-body=true, timeout=60 ,tag=ChatGPTPlusCrack_1
